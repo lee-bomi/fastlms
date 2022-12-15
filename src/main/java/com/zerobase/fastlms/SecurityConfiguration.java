65a3654878798615f44cb967138cbc8993089de2
emmababy <bomvll@naver.com>
2022-12-15 22:11:28 +0900

    시큐리티 설정 성공 - post요청 및 시큐리티 에러 해결

--- src/main/java/com/zerobase/fastlms/SecurityConfiguration.java
+++ src/main/java/com/zerobase/fastlms/SecurityConfiguration.java
@@ -4,8 +4,6 @@ import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
-import org.springframework.web.servlet.config.annotation.CorsRegistry;
-import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @EnableWebSecurity
 @Configuration
